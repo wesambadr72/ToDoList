bab235ee9064ddea2a91dd9bad1ca25fec9d1634
wesm DESKTOP <wbadr60@gmail.com>
2025-01-27 23:35:31 +0300

    flask

--- LearnFlask/start.py
+++ LearnFlask/start.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask , render_template , url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE-URI'] = 'sqlite:///test.db'
+db=SQLAlchemy(app)
 
+class ToDo(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    content=db.column(db.string(150), nullable=False)
+    date_Created=db.column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+     
 @app.route('/')
-def home():
-    return "Hi i'm Wesam \nWow Great! It's working"
+def index():
+    return render_template('index.html')
 
 if __name__=='__main__':
      app.run(debug=True)
