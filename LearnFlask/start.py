1af84833d4bc65a2d68c98ade46758f169eb2898
wesm DESKTOP <wbadr60@gmail.com>
2025-01-29 02:06:08 +0300

    Implement task management features with MongoDB integration and enhance UI styling

--- LearnFlask/start.py
+++ LearnFlask/start.py
@@ -1,22 +1,30 @@
-from flask import Flask , render_template , url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask , render_template , url_for , request , redirect
+from flask_pymongo import PyMongo
 from datetime import datetime
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE-URI'] = 'sqlite:///test.db'
-db=SQLAlchemy(app)
-
-class ToDo(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    content=db.column(db.string(150), nullable=False)
-    date_Created=db.column(db.DateTime, default=datetime.now())
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
+#Create a database connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Learn"
+db=PyMongo(app).db
      
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    Task_collection = db.Tasks
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task ={'content':task_content ,'date_Created':datetime.now()}
+
+        try:
+            Task_collection.insert_one(new_task)
+            return redirect('/')
+        except:
+            return 'Error occurred while inserting task'
+    else:
+        tasks = Task_collection.find(sort=[('date_Created',-1)])
+        return render_template('index.html',tasks = tasks)
+        
+    
+    
 
 if __name__=='__main__':
      app.run(debug=True)
