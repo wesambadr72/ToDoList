9badff25be0062c132edc195ad21cf63c917b289
wesm DESKTOP <wbadr60@gmail.com>
2025-01-31 16:28:44 +0300

    add func

--- LearnFlask/start.py
+++ LearnFlask/start.py
@@ -1,6 +1,7 @@
 from flask import Flask , render_template , url_for , request , redirect
 from flask_pymongo import PyMongo
 from datetime import datetime
+from bson.objectid import ObjectId
 
 app=Flask(__name__)
 #Create a database connection
@@ -12,7 +13,7 @@ def index():
     Task_collection = db.Tasks
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task ={'content':task_content ,'date_Created':datetime.now()}
+        new_task ={'content':task_content ,'date_Created':datetime.now(), 'completed':False}
 
         try:
             Task_collection.insert_one(new_task)
@@ -22,8 +23,47 @@ def index():
     else:
         tasks = Task_collection.find(sort=[('date_Created',-1)])
         return render_template('index.html',tasks = tasks)
-        
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    Task_collection = db.Tasks
+    try:
+        Task_collection.delete_one({'_id':ObjectId(id)})
+        return redirect('/')
+    except: 
+        return 'Error occurred while deleting task'
     
+@app.route('/mark_completed/<id>', methods=['POST'])
+def mark_completed(id):
+    Task_collection = db.Tasks
+    task= Task_collection.find_one({'_id': ObjectId(id)})
+    new_status = not task.get('completed', False)
+    try:
+        Task_collection.update_one({'_id': ObjectId(id)}, {'$set':{'completed':new_status}})
+        if new_status:
+            Task_collection.delete_one({'_id': ObjectId(id)})
+        return redirect('/')
+    except:
+        return 'Error occurred while marking task as completed'
+
+@app.route('/update/<id>' , methods=["POST", "GET"])
+def update(id):
+    task_collection = db.Tasks
+    if request.method == 'POST':
+        updated_content = request.form['content']
+        # Update the task in the database
+        try:
+            task_collection.update_one({'_id':ObjectId(id)},{'$set':{'content':updated_content}})
+            return redirect('/')
+        except:
+            return 'Error occurred while updating task'
+    else:
+        task=task_collection.find_one({'_id':ObjectId(id)})
+        return render_template('update.html',task=task)
+        
+
+
     
 
 if __name__=='__main__':
