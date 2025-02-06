e4e5801830844cc4380f3b372af378d4b297db43
wesm DESKTOP <wbadr60@gmail.com>
2025-02-06 19:09:55 +0300

    Add some details

--- LearnFlask/start.py
+++ LearnFlask/start.py
@@ -12,15 +12,15 @@ db=PyMongo(app).db
 def index():
     Task_collection = db.Tasks
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task ={'content':task_content ,'date_Created':datetime.now(), 'completed':False}
+        task_content = request.form['content'] # get the task content
+        new_task ={'content':task_content ,'date_Created':datetime.now(), 'completed':False} # create a new task
 
         try:
-            Task_collection.insert_one(new_task)
+            Task_collection.insert_one(new_task) # add it to database
             return redirect('/')
         except:
             return 'Error occurred while inserting task'
-    else:
+    else:# return the rest of tasks (method = GET)
         tasks = Task_collection.find(sort=[('date_Created',-1)])
         return render_template('index.html',tasks = tasks)
 
@@ -28,6 +28,7 @@ def index():
 @app.route('/delete/<id>')
 def delete(id):
     Task_collection = db.Tasks
+    #delete item from database
     try:
         Task_collection.delete_one({'_id':ObjectId(id)})
         return redirect('/')
@@ -37,12 +38,12 @@ def delete(id):
 @app.route('/mark_completed/<id>', methods=['POST'])
 def mark_completed(id):
     Task_collection = db.Tasks
-    task= Task_collection.find_one({'_id': ObjectId(id)})
-    new_status = not task.get('completed', False)
-    try:
+    task= Task_collection.find_one({'_id': ObjectId(id)}) #find the task from database
+    new_status = not task.get('completed', False) #mark the task as completed
+    try: #update the task status in database
         Task_collection.update_one({'_id': ObjectId(id)}, {'$set':{'completed':new_status}})
-        if new_status:
-            Task_collection.delete_one({'_id': ObjectId(id)})
+        if new_status: #check if the task is completed
+            Task_collection.delete_one({'_id': ObjectId(id)}) # delete the task from database
         return redirect('/')
     except:
         return 'Error occurred while marking task as completed'
